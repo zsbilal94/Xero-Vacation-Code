f26d83d1da5ccf1c0fae00289beac39f0ce4e474
unknown <zainab.shoaib@XLW-5CG43810FX.inside.xero.com>
2016-12-07 14:20:36 +1100

    Initail Commit

---
+++ FizzBuzz/FizzBuzz/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int range = 100;
+            int fizz = 3;
+            int buzz = 5;
+            for (int n = 1; n <= range; n++)
+            {
+   
+                if (n % fizz == 0 && n % buzz == 0)
+                {
+                    System.Console.WriteLine("FizzBuzz");
+                }
+
+                else if (n % fizz == 0)
+                {
+                    System.Console.WriteLine("Fizz");
+                }
+                
+                else if (n % buzz == 0)
+                {
+                    System.Console.WriteLine("Buzz");
+                }
+
+                else
+                {
+                    System.Console.WriteLine("{0}", n);
+                }
+            }
+            System.Console.Read();
+        }
+    }
+}
